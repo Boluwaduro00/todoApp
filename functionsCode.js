45ac6675446814f1fee9c41ea07a86e7452f13ef
Boluwaduro00 <adebajobolu@gmail.com>
2020-04-16 20:24:19 +0100

    modifications

--- functionsCode.js
+++ functionsCode.js
@@ -1,25 +1,32 @@
-const toDoItems = [
-   // {name: '', completed: false}
-];
+const toDoItems = ["teach", "learn "];
+   // {name: '', completed: false}];
 
 function addNewItem(item){
     toDoItems.push(item);  
 }
 
 function deleteItem(item){
-    for(i = 0; i < toDoItems.length; i++){
+    for(i = 0; i <= toDoItems.length; i++){
         if (item == i){
-            delete toDoItems[i];
+            toDoItems.splice(i,1);
         }
+
+        return toDoItems;
+    }
 }
+
+
 function markItem(){
 
 }
+
+
 function listItem(){
 
 }
 
 
-addNewItem({name: 'write code', completed:false})
+addNewItem({name: 'write code', completed:false});
 
-console.log(toDoItems)
+console.log(toDoItems);
+console.log(deleteItem(0));
