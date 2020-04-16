4248d49783157cfc32929528583d82e4d19746fe
Boluwaduro00 <adebajobolu@gmail.com>
2020-04-16 17:51:28 +0100

    changes made to functions

--- functionsCode.js
+++ functionsCode.js
@@ -1,9 +1,25 @@
-var toDoItems = [];
+const toDoItems = [
+   // {name: '', completed: false}
+];
 
-function addNewItem(list){
-    list.push();  
+function addNewItem(item){
+    toDoItems.push(item);  
 }
 
-function deleteItem(list){
-    delete list[0];
+function deleteItem(item){
+    for(i = 0; i < toDoItems.length; i++){
+        if (item == i){
+            delete toDoItems[i];
+        }
 }
+function markItem(){
+
+}
+function listItem(){
+
+}
+
+
+addNewItem({name: 'write code', completed:false})
+
+console.log(toDoItems)
